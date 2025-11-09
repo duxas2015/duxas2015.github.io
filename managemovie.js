0391f72b1324ad2bf17087b305e1bf36eda85b53
evgeniinekorkin <evgenii.nekorkin@billingplatform.com>
2025-11-09 11:34:50 -0800

    no message

--- managemovie.js
+++ managemovie.js
@@ -598,9 +598,11 @@ window.onload = function() {
 			  document.getElementById("idRewind2Second").addEventListener('click', ( event ) => { backMoving (2); } );
 			  document.getElementById("idRewind3Second").addEventListener('click', ( event ) => { backMoving (3); } );
 			  document.getElementById("idRewindUntilNextSub").addEventListener('click', ( event ) => { getToTheClosestSubtitle(); } );
-			  // document.getElementById("idSetRusSub").addEventListener('click', ( event ) => { /* REMOVED: Old 'S' button handler */ } );
-			  
-			  // Новая функциональность для кнопок сдвига субтитров (сохранена и используется)
+			  document.getElementById("idSetRusSub").addEventListener('click', ( event ) => { 
+				  document.getElementsByTagName('video')[0].textTracks[findRussianSubtitleTrackIndex()].oncuechange = f;
+				  document.getElementsByTagName('video')[0].textTracks[findEnglishSubtitleTrackIndex()].oncuechange = f_eng;
+				  video = document.getElementsByTagName('video')[0];
+				} );
 			  document.getElementById("idMoveEngSubBackward").addEventListener('click', ( event ) => { 
 				  shiftTextTrack(findEnglishSubtitleTrack(), -0.2 );
 				  shiftEnglishSubtitle-= 0.2;
